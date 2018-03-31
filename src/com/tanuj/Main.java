570b3576f9c92ce29b27b8757091d00d178d02f2
Tanuj Nayak <tanujnay112@live.com>
2018-03-31 02:34:46 -0400

    Added subscriber functionality

--- src/com/tanuj/Main.java
+++ src/com/tanuj/Main.java
@@ -2,25 +2,59 @@ package com.tanuj;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamMotionDetector;
+import com.github.sarxos.webcam.WebcamMotionEvent;
+import com.github.sarxos.webcam.WebcamMotionListener;
+import redis.clients.jedis.Jedis;
 
-import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.nio.ByteBuffer;
+import java.util.Base64;
 
-public class Main {
+public class Main implements WebcamMotionListener {
 
-    public static void main(String[] args) {
-	// write your code here
-      WebcamMotionDetector detector = new WebcamMotionDetector(
-          Webcam.getDefault(), 20, 100);
-      detector.setInterval(10);
-      detector.start();
-      while(true){
-        if(detector.isMotion()){
-          frameChange();
-        }
-      }
+  final static String location = "";
+  final static String channelName = "";
+  static Dimension length;
+  static Webcam w;
+
+  public static void publish(byte[] b){
+    Jedis jedis = new Jedis(location);
+
+    jedis.publish(channelName, new String(Base64.getEncoder()
+        .encodeToString(b)));
+  }
+
+  public Main(){
+    WebcamMotionDetector detector = new WebcamMotionDetector(w);
+    detector.setInterval(100);
+    detector.addMotionListener(this);
+    detector.start();
+    length = w.getViewSize();
+  }
+
+  public static void main(String[] args) {
+    w = Webcam.getDefault();
+    System.out.println("STARTING");
+    Main m = new Main();
+    while(true){
+
     }
+  }
 
   public static void frameChange() {
+    System.out.println("CHANGED");
+    ByteBuffer bb = w.getImageBytes();
+    byte[] bytes = new byte[length.height*length.width];
+    synchronized(bb){
+      bb.rewind();
+      bb.get(bytes);
+      bb.rewind();
+      publish(bytes);
+    }
+  }
 
+  public void motionDetected(WebcamMotionEvent webcamMotionEvent) {
+    System.out.println("WHAT THE");
+    frameChange();
   }
 }
