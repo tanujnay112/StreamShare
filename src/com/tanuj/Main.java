59b44b7cb50d0934a6a2b501dadb4b3c3e2d4b33
Cyrus Daruwala <Cyrus@Cyruss-MacBook-Pro.local>
2018-05-20 17:02:03 -0700

    Changed indentation

--- src/com/tanuj/Main.java
+++ src/com/tanuj/Main.java
@@ -14,81 +14,80 @@ import java.nio.ByteBuffer;
 import java.util.Base64;
 
 public class Main extends JApplet implements WebcamMotionListener {
+    static String location = "";
+    final static String channelName = "slides";
+    static Dimension length;
+    static Webcam w;
 
-  static String location = "";
-  final static String channelName = "slides";
-  static Dimension length;
-  static Webcam w;
+    static WebcamPanel display;
+    final static boolean RELAY = false;
 
-  static WebcamPanel display;
-  final static boolean RELAY = false;
-
-  public static void publish(String s){
-    Jedis jedis = new Jedis(location);
-    //String s = Base64.getEncoder().encodeToString(b);
-    //System.out.println(s);
-    //s = String.format("%d;%d;%s", d.height, d.width, s);
-    //System.exit(0);
-    jedis.publish(channelName, s);
-  }
-
-  public Main(){
-    WebcamMotionDetector detector = new WebcamMotionDetector(w);
-    detector.setInterval(100);
-    detector.addMotionListener(this);
-    detector.setAreaThreshold(0.3);
-    detector.start();
-    length = w.getViewSize();
-  }
-
-  public static void main(String[] args) {
-    location = args[0];
-    w = Webcam.getDefault();
-    Dimension[] nonStandardResolutions = new Dimension[] {
-        WebcamResolution.HD.getSize(),
-    };
-    w.setCustomViewSizes(nonStandardResolutions);
-    w.setViewSize(WebcamResolution.HD.getSize());
-    length = w.getViewSize();
-    if(RELAY){
-      display = new WebcamPanel(w, false);
-      ImageDisplay disp = new ImageDisplay(length.width, length.height, w);
-    }
-    System.out.println("STARTING");
-    Main m = new Main();
-    while(true){
-
-    }
-  }
-
-  public void frameChange(BufferedImage image) {
-
-    //LOG.info("New image from {}", webcam);
-
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    try {
-      ImageIO.write(image, "JPG", baos);
-    } catch (IOException e) {
-      //LOG.error(e.getMessage(), e);
+    public static void publish(String s){
+        Jedis jedis = new Jedis(location);
+        //String s = Base64.getEncoder().encodeToString(b);
+        //System.out.println(s);
+        //s = String.format("%d;%d;%s", d.height, d.width, s);
+        //System.exit(0);
+        jedis.publish(channelName, s);
     }
 
-    String base64 = null;
-    try {
-      base64 = new String(Base64.getEncoder().encode(baos.toByteArray()), "UTF8");
-    } catch (UnsupportedEncodingException e) {
-      //LOG.error(e.getMessage(), e);
+    public Main(){
+        WebcamMotionDetector detector = new WebcamMotionDetector(w);
+        detector.setInterval(100);
+        detector.addMotionListener(this);
+        detector.setAreaThreshold(0.3);
+        detector.start();
+        length = w.getViewSize();
     }
 
-    //Map<String, Object> message = new HashMap<String, Object>();
-    //message.put("type", "image");
-    //message.put("webcam", webcam.getName());
-    //message.put("image", base64);
+    public static void main(String[] args) {
+        location = args[0];
+        w = Webcam.getDefault();
+        Dimension[] nonStandardResolutions = new Dimension[] {
+            WebcamResolution.HD.getSize(),
+        };
+        w.setCustomViewSizes(nonStandardResolutions);
+        w.setViewSize(WebcamResolution.HD.getSize());
+        length = w.getViewSize();
+        if(RELAY){
+          display = new WebcamPanel(w, false);
+          ImageDisplay disp = new ImageDisplay(length.width, length.height, w);
+        }
+        System.out.println("STARTING");
+        Main m = new Main();
+        while(true){
 
-    publish(base64);
-  }
+        }
+    }
+
+    public void frameChange(BufferedImage image) {
+
+        //LOG.info("New image from {}", webcam);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+          ImageIO.write(image, "JPG", baos);
+        } catch (IOException e) {
+          //LOG.error(e.getMessage(), e);
+        }
+
+        String base64 = null;
+        try {
+          base64 = new String(Base64.getEncoder().encode(baos.toByteArray()), "UTF8");
+        } catch (UnsupportedEncodingException e) {
+          //LOG.error(e.getMessage(), e);
+        }
+
+        //Map<String, Object> message = new HashMap<String, Object>();
+        //message.put("type", "image");
+        //message.put("webcam", webcam.getName());
+        //message.put("image", base64);
+
+        publish(base64);
+    }
 
 
-  /*public static void frameChange() {
+    /*public static void frameChange() {
     System.out.println("CHANGED");
     ByteBuffer bb = w.getImageBytes();
     System.out.printf("height: %d, width: %d\n", length.height, length.width);
@@ -100,17 +99,17 @@ public class Main extends JApplet implements WebcamMotionListener {
       bb.rewind();
       publish(bytes, length);
     }
-  }*/
+    }*/
 
-  public void motionDetected(WebcamMotionEvent webcamMotionEvent) {
-    System.out.println("WHAT THE");
-    frameChange(w.getImage());
-  }
-
-  private static void updateFrame(BufferedImage bytes){
-    if(!RELAY){
-      return;
+    public void motionDetected(WebcamMotionEvent webcamMotionEvent) {
+        System.out.println("WHAT THE");
+        frameChange(w.getImage());
     }
-    //display.setImage(bytes);
-  }
+
+    private static void updateFrame(BufferedImage bytes){
+        if(!RELAY){
+            return;
+        }
+        //display.setImage(bytes);
+    }
 }
--- src/com/tanuj/Subscriber.java
+++ src/com/tanuj/Subscriber.java
@@ -60,6 +60,7 @@ public class Subscriber {
                 }
 
             }, channel);
+
         while (true) {
 
         }
